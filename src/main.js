5f0b6dfa8748bbe97aa44c7835b1742e2f825db4
xiaosongfu <xiaosong.fu@outlook.com>
2019-09-27 14:20:00 +0800

    add axios

--- src/main.js
+++ src/main.js
@@ -4,13 +4,15 @@ import router from './router'
 import store from './store'
 
 import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
-axios.
-Vue.prototype.$axios = axios
+axios.defaults.baseURL = ""
+Vue.use(VueAxios, axios)
+// Vue.prototype.$axios = axios
 
 new Vue({
   router,
