a03879775702fdfee2d8d6e57cde30e549bb4d42
xiaosongfu <xiaosong.fu@outlook.com>
2019-09-27 14:11:36 +0800

    add vue-router vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
+
+import router from './router'
+import store from './store'
+
+import axios from 'axios'
+
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+axios.
+Vue.prototype.$axios = axios
+
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
